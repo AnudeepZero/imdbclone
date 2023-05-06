1ef0494eaa3eed087f1be99638041bce4bc40656
AnudeepZero <anudeepsambaraju@gmail.com>
2023-05-06 13:38:48 +0530

    Navbar

---
+++ src/app/about/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function page() {
+    return (
+        <div className='max-w-6xl mx-auto space-y-4 p-4'>
+            <h1 className='text-2xl font-medium text-amber-600' >About</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Purus sit amet volutpat consequat. Vitae ultricies leo integer malesuada nunc vel. Cras sed felis eget velit. Etiam erat velit scelerisque in dictum non consectetur. Egestas maecenas pharetra convallis posuere morbi leo. Et malesuada fames ac turpis. Feugiat vivamus at augue eget arcu dictum varius duis at. In arcu cursus euismod quis viverra nibh cras. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing. Sed id semper risus in hendrerit gravida. Posuere lorem ipsum dolor sit amet consectetur. Consequat id porta nibh venenatis cras. Potenti nullam ac tortor vitae purus faucibus ornare suspendisse. Scelerisque in dictum non consectetur. Cras semper auctor neque vitae tempus quam pellentesque nec nam. Arcu risus quis varius quam quisque id diam.
+
+            </p>
+            <p>Enim nunc faucibus a pellentesque. Nec tincidunt praesent semper feugiat nibh sed. Arcu vitae elementum curabitur vitae nunc sed. Adipiscing at in tellus integer feugiat. Scelerisque purus semper eget duis. Mollis aliquam ut porttitor leo a diam sollicitudin. Id volutpat lacus laoreet non curabitur gravida. Nisi scelerisque eu ultrices vitae. Cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum. Lectus magna fringilla urna porttitor. Nisl nunc mi ipsum faucibus vitae.
+
+            </p>
+            <p>Amet nisl suscipit adipiscing bibendum est ultricies integer quis auctor. At volutpat diam ut venenatis tellus. Convallis convallis tellus id interdum velit. Nulla facilisi etiam dignissim diam quis enim lobortis. Malesuada pellentesque elit eget gravida cum sociis. Consequat id porta nibh venenatis. Odio euismod lacinia at quis risus sed vulputate odio. Non consectetur a erat nam at. Risus quis varius quam quisque id diam vel quam elementum. Volutpat maecenas volutpat blandit aliquam etiam erat velit scelerisque in. Elementum integer enim neque volutpat ac tincidunt. Vitae justo eget magna fermentum. Convallis a cras semper auctor neque. Egestas tellus rutrum tellus pellentesque eu tincidunt tortor aliquam. Rhoncus urna neque viverra justo nec ultrices dui sapien eget. Ornare quam viverra orci sagittis eu volutpat odio facilisis mauris. Enim neque volutpat ac tincidunt vitae semper quis. Lorem donec massa sapien faucibus et molestie. Sed risus ultricies tristique nulla aliquet. Posuere morbi leo urna molestie.
+
+            </p>
+        </div>
+    )
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import Header from '@/components/Header'
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Provider from './Provider'
+import NavBar from '@/components/NavBar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -22,6 +23,9 @@ export default function RootLayout({ children }) {
           {/* Header component */}
           <Header />
 
+          {/* Navbar */}
+          <NavBar />
+          {/* body */}
           {children}
         </Provider>
 
